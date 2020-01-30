3301b4a47c12046f49f44310905f99ab07661e95
Eduardo <edualvsald@hotmail.com>
2020-01-30 18:53:22 -0300

    test(services/SismosServiceTest): upload the test
    Test for obtain the data from the ChileAlertaApi

---
+++ app/src/test/java/cl/ucn/disc/dsm/sismosapp/services/SismosServiceTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright [2020] [Eduardo Alvarez S]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.ucn.disc.dsm.sismosapp.services;
+
+
+import cl.ucn.disc.dsm.sismosapp.model.Sismo;
+import cl.ucn.disc.dsm.sismosapp.services.sismosapi.SismosApiService;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The test of the SismosService.
+ *
+ * @author Eduardo Alvarez S.
+ */
+public class SismosServiceTest {
+
+  private static final Logger log = LoggerFactory.getLogger(SismosService.class);
+
+@Test
+public void testGetSismosApi(){
+
+
+    log.debug("Testing the SismosApiService, requesting {} Sismos.");
+
+  // The Sismos service.
+  final SismosService sismosService = new SismosApiService();
+
+  // The List of Sismos.
+  final List<Sismo> sismos = sismosService.getSismos();
+
+    Assertions.assertNotNull(sismos);
+    for (final Sismo noticia : sismos) {
+    log.debug("Noticia: {}.", sismos);
+  }
+
+    log.debug("Done.");
+
+}
+
+
+}
